c66eb5c288a41cec380315da696c56518ddf960d
Sadequl Islam Mithun <sim756@outlook.com>
2025-10-05 10:21:57 +0600

    Add project files.

---
+++ sim756.TOTP/Base32Encoding.cs
@@ -0,0 +1,102 @@
+﻿namespace sim756.TOTP
+{
+	internal class Base32Encoding
+	{
+		public static byte[] ToBytes(string value)
+		{
+			value = value.TrimEnd('=');
+
+			int byteCount = value.Length * 5 / 8;
+			byte[] result = new byte[byteCount];
+			int arrayIndex = 0;
+			byte remainingBits = 8;
+			byte currentByte = 0;
+			int mask = 0;
+
+
+			int CharToAscii(char character)
+			{
+				int asciiCharValue = (int)character;
+
+				return asciiCharValue switch
+				{
+					< 91 and > 64 => asciiCharValue - 65,
+					< 56 and > 49 => asciiCharValue - 24,
+					< 123 and > 96 => asciiCharValue - 97,
+					_ => throw new ArgumentException("Invalid Base32 character!")
+				};
+			}
+
+			for (int i = 0; i < value.Length; i++)
+			{
+				int currentValue = CharToAscii(value[i]);
+
+				if (remainingBits > 5)
+				{
+					mask = currentValue << (remainingBits - 5);
+					currentByte = (byte)(currentByte | mask);
+					remainingBits -= 5;
+				}
+				else
+				{
+					mask = currentValue >> (5 - remainingBits);
+					currentByte = (byte)(currentByte | mask);
+					result[arrayIndex++] = currentByte;
+					currentByte = (byte)(currentValue << (3 + remainingBits));
+					remainingBits += 3;
+				}
+			}
+
+			if (arrayIndex != byteCount)
+			{
+				result[arrayIndex] = currentByte;
+			}
+
+			return result;
+		}
+
+		public static string ToString(byte[] value)
+		{
+			int charCount = (int)Math.Ceiling(value.Length / 5d) * 8;
+			char[] result = new char[charCount];
+			int arrayIndex = 0;
+			byte nextChar = 0;
+			byte remainingBits = 5;
+
+			char AsciiToChar(byte ascii)
+			{
+				return ascii switch
+				{
+					< 26 => (char)(ascii + 65),
+					< 32 => (char)(ascii + 24),
+					_ => throw new ArgumentException("Invalid Base32 value!")
+				};
+			}
+
+			for (int i = 0; i < value.Length; i++)
+			{
+				byte currentByte = value[i];
+				nextChar = (byte)(nextChar | (currentByte >> (8 - remainingBits)));
+				result[arrayIndex++] = AsciiToChar(nextChar);
+
+				if (remainingBits < 4)
+				{
+					nextChar = (byte)((currentByte >> (3 - remainingBits)) & 31);
+					result[arrayIndex++] = AsciiToChar(nextChar);
+					remainingBits += 5;
+				}
+
+				remainingBits -= 3;
+				nextChar = (byte)((currentByte << remainingBits) & 31);
+			}
+
+			if (arrayIndex != charCount)
+			{
+				result[arrayIndex++] = AsciiToChar(nextChar);
+				while (arrayIndex != charCount) result[arrayIndex++] = '=';
+			}
+
+			return new string(result);
+		}
+	}
+}
---
+++ sim756.TOTP/TOTPGenerator.cs
@@ -0,0 +1,104 @@
+﻿using sim756.TOTP;
+using System;
+using System.Security.Cryptography;
+
+namespace sim756.HOTP
+{
+    public class TOTPGenerator
+    {
+        private const long UnixEpochTicks = 621355968000000000L;
+        private const long TicksToSeconds = 10000000L;
+
+        private const int DefaultLength = 6;
+        private const int DefaultSteps = 30;
+
+        public int Length { get; set; } = DefaultLength;
+        public long Steps { get; set; } = DefaultSteps;
+
+        private byte[] key;
+        private string keyString;
+
+        public byte[] Key
+        {
+            get => key;
+            set
+            {
+                key = value;
+                keyString = Base32Encoding.ToString(value);
+            }
+        }
+
+        public string KeyString
+        {
+            get => keyString;
+            set
+            {
+                keyString = value;
+                Key = Base32Encoding.ToBytes(value);
+            }
+        }
+
+        public TOTPGenerator()
+        {
+        }
+
+        public TOTPGenerator(byte[] key, int hotpLength = DefaultLength, int hotpStep = DefaultSteps)
+        {
+            Key = key;
+            Length = hotpLength;
+            Steps = hotpStep;
+        }
+
+        public TOTPGenerator(string key, int hotpLength = DefaultLength, int hotpStep = DefaultSteps)
+        {
+            Key = Base32Encoding.ToBytes(key);
+            Length = hotpLength;
+            Steps = hotpStep;
+        }
+
+        public string Compute(string key, DateTime? dateTime = null)
+        {
+            return Compute(Base32Encoding.ToBytes(key), dateTime);
+        }
+
+        public string Compute(byte[] key = null, DateTime? dateTime = null)
+        {
+            byte[] hmacComputedHash =
+                new HMACSHA1(key ?? Key).ComputeHash(
+                    GetBigEndianBytes(GetTimeStepFromTimeStamp(dateTime ?? DateTime.UtcNow)));
+
+            int offset = hmacComputedHash[hmacComputedHash.Length - 1] & 0x0F;
+            int otp = (hmacComputedHash[offset] & 0x7f) << 24
+                      | (hmacComputedHash[offset + 1] & 0xff) << 16
+                      | (hmacComputedHash[offset + 2] & 0xff) << 8
+                      | (hmacComputedHash[offset + 3] & 0xff) % 1000000;
+
+            return TruncateANDPad(otp, Length);
+        }
+
+        public long RemainingSeconds()
+        {
+            return Steps - ((DateTime.UtcNow.Ticks - UnixEpochTicks) / TicksToSeconds) % Steps;
+        }
+
+        private byte[] GetBigEndianBytes(long value)
+        {
+            byte[] bytes = BitConverter.GetBytes(value);
+            Array.Reverse(bytes);
+            return bytes;
+        }
+
+        private long GetTimeStepFromTimeStamp(DateTime timestamp)
+        {
+            long unixTimestamp = (timestamp.Ticks - UnixEpochTicks) / TicksToSeconds;
+            long window = unixTimestamp / Steps;
+            return window;
+        }
+
+        private string TruncateANDPad(long input, int digitCount)
+        {
+            int truncatedValue = ((int)input % (int)Math.Pow(10, digitCount));
+            return truncatedValue.ToString().PadLeft(digitCount, '0');
+        }
+    }
+}
